f57c57bc3e05ebc9b38544c742d2e97ecfbd3122
U-MSI\basha <bashikr@gmail.com>
2021-01-02 02:04:16 +0100

    Added cache and config folders to /test repository

--- test/config/geoToken.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Config file.
- */
-return [
-    "message" => "",
-];
---
+++ test/config/router/102_tagsController.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Routes for controller.
+ */
+return [
+    "routes" => [
+        [
+            "info" => "All Tags",
+            "mount" => "tags",
+            "handler" => "Anax\Tags\TagsController",
+        ],
+    ]
+];
